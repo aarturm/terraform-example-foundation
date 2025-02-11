894b256b3c036ba0a684271e0a39e466c11e05a5
aarturm <52535110+aarturm@users.noreply.github.com>
2025-02-11 07:04:28 +0100

    destroy method corrected
    from PATCH to PUT

--- 1-org/modules/centralized-logging/main.tf
+++ 1-org/modules/centralized-logging/main.tf
@@ -208,7 +208,7 @@ resource "terracurl_request" "exclude_external_logs" {
 EOF
 
 destroy_url    = "https://logging.googleapis.com/v2/projects/${var.logging_destination_project_id}/sinks/_Default?updateMask=exclusions"
-destroy_method = "PATCH"
+destroy_method = "PUT"
 destroy_request_body = <<EOF
 {
   "exclusions": [],
